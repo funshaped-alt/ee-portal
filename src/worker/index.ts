ac3ebac3279b124bb623974a2d88d3b1cf3aa5c7
Funshaped <75537163+sena5641@users.noreply.github.com>
2026-06-27 19:39:23 +0300

    test

--- src/worker/index.ts
+++ src/worker/index.ts
@@ -1,6 +1,6 @@
 import { Hono } from "hono";
 const app = new Hono<{ Bindings: Env }>();
 
-app.get("/api/", (c) => c.json({ name: "Cloudflare" }));
+app.get("/api/", (c) => c.json({ name: "lets make this a bit cooler" }));
 
 export default app;
